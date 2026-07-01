ecb27b2fb3e604581a7ecaaddc2cea0f7ba93071
dtfgenetics <green801028@gmail.com>
2026-07-01 05:06:48 -0500

    Add spawn plan 01

---
+++ prototypes/web-fps-v2/src/spawn-plans/spawn-plan-01.js
@@ -0,0 +1,7 @@
+export const SPAWN_PLAN_01 = {
+  level: "The Veg Lab",
+  threats: ["spider_mite_swarm", "powdery_mildew_ghoul", "nute_burn_sprayer"],
+  pickups: ["ph_blaster", "cure_jar_health", "green_keycard", "light_ammo_box"],
+  lore: ["note_veg_01"],
+  objective: "green_keycard_route"
+};
