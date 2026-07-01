68ac1bece9071bea3a1d981c4a4bdb192bf97dcc
dtfgenetics <green801028@gmail.com>
2026-07-01 05:07:48 -0500

    Add spawn plan 08

---
+++ prototypes/web-fps-v2/src/spawn-plans/spawn-plan-08.js
@@ -0,0 +1,7 @@
+export const SPAWN_PLAN_08 = {
+  level: "Seed Bank Siege",
+  threats: ["bud_rot_brute", "compliance_drone", "nute_burn_sprayer"],
+  pickups: ["gold_keycard", "trichome_reaper", "rare_seed_pack"],
+  lore: ["note_seed_01"],
+  objective: "seed_bank_route"
+};
