60e2ad1d49287f59e50d6153006cba5fdede67f7
dtfgenetics <green801028@gmail.com>
2026-07-01 04:13:38 -0500

    Add V2 inventory rules

---
+++ prototypes/web-fps-v2/src/inventory-rules.js
@@ -0,0 +1,14 @@
+export const INVENTORY_RULES = {
+  maxHealth: 100,
+  maxArmor: 100,
+  maxSpecial: 100,
+  ammoCaps: {
+    light: 120,
+    heavy: 40,
+    fuel: 80,
+    grenade: 8,
+    special: 3
+  },
+  keyTypes: ["green", "purple", "gold"],
+  scoreItems: ["rare_seed_pack", "golden_nug"]
+};
