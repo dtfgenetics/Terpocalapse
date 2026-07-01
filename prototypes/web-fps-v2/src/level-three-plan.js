5146c784937ea6811c5ce3d4228efa4a9416a0a8
dtfgenetics <green801028@gmail.com>
2026-07-01 04:12:23 -0500

    Add V2 level three plan

---
+++ prototypes/web-fps-v2/src/level-three-plan.js
@@ -0,0 +1,17 @@
+export const LEVEL_THREE_PLAN = {
+  title: "Nutrient Reservoir",
+  mood: "wet service tunnels and feed tanks",
+  rooms: [
+    "Reservoir Entry",
+    "Feed Tank Hall",
+    "Pipe Switch Room",
+    "Overflow Walkway",
+    "Service Lift"
+  ],
+  goals: [
+    "Find the feed control switch",
+    "Cross the reservoir path",
+    "Reach the service lift"
+  ],
+  rewards: ["CO2 Burst Rifle", "Light Ammo Box", "Grow Light Overdrive"]
+};
