606e728fb172a7b9f9805026d2d95191b6b045cb
dtfgenetics <green801028@gmail.com>
2026-06-30 21:57:33 -0500

    Add V2 progression structure

---
+++ prototypes/web-fps-v2/src/progression.js
@@ -0,0 +1,13 @@
+export const PROGRESSION = {
+  episodeTitle: "Grow Room From Hell",
+  requiredFirstObjective: "Green Keycard",
+  firstExitName: "Exit Chamber",
+  longTermObjective: "Seed Vault",
+  routeOrder: [
+    "start room",
+    "first pickup",
+    "green keycard",
+    "green door",
+    "exit chamber"
+  ]
+};
