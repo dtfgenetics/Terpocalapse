c616175bdf4ea1492ef25ed0d04cea3836618952
dtfgenetics <green801028@gmail.com>
2026-07-01 05:07:05 -0500

    Add spawn plan 03

---
+++ prototypes/web-fps-v2/src/spawn-plans/spawn-plan-03.js
@@ -0,0 +1,7 @@
+export const SPAWN_PLAN_03 = {
+  level: "Nutrient Reservoir",
+  threats: ["nute_burn_sprayer", "powdery_mildew_ghoul"],
+  pickups: ["co2_burst_rifle", "light_ammo_box", "grow_light_overdrive"],
+  lore: ["note_nute_01"],
+  objective: "reservoir_route"
+};
