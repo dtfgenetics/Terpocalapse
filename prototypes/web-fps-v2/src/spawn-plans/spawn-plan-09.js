dfe4b465b526a30d4a30fd753caac0d8b98dc32a
dtfgenetics <green801028@gmail.com>
2026-07-01 05:07:54 -0500

    Add spawn plan 09

---
+++ prototypes/web-fps-v2/src/spawn-plans/spawn-plan-09.js
@@ -0,0 +1,7 @@
+export const SPAWN_PLAN_09 = {
+  level: "Mold Mother's Den",
+  threats: ["mold_mother", "spider_mite_swarm", "nute_burn_sprayer"],
+  pickups: ["trichome_reaper", "golden_nug", "grow_light_overdrive"],
+  lore: ["note_final_01"],
+  objective: "vault_core_route"
+};
