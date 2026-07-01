abfb0d4e1c52bc7e1e0d18964e690e2b87f745fb
dtfgenetics <green801028@gmail.com>
2026-07-01 05:05:39 -0500

    Add V2 map 02

---
+++ prototypes/web-fps-v2/src/maps/map-02-clone-room.js
@@ -0,0 +1,19 @@
+export const MAP_02_CLONE_ROOM = {
+  id: "map_02_clone_room",
+  title: "Clone Room Panic",
+  tileSize: 64,
+  layout: [
+    "############",
+    "#P....#....#",
+    "#.....#....#",
+    "#..........#",
+    "###..###...#",
+    "#....K.....#",
+    "#..........#",
+    "#..S....D..#",
+    "#........X.#",
+    "############"
+  ],
+  start: { x: 1.5, y: 1.5, angle: 0 },
+  notes: "Tight clone-rack route with a secret supply shelf and exit door."
+};
