ab545f69add941af9c67e4d2c028795d9e71cf9d
dtfgenetics <green801028@gmail.com>
2026-07-01 04:15:28 -0500

    Add V2 reward table

---
+++ prototypes/web-fps-v2/src/reward-table.js
@@ -0,0 +1,11 @@
+export const REWARD_TABLE = {
+  The_Veg_Lab: ["ph_blaster", "green_keycard"],
+  Clone_Room_Panic: ["neem_cannon"],
+  Nutrient_Reservoir: ["co2_burst_rifle"],
+  Spider_Mite_Sector: ["kief_armor_bonus"],
+  Flower_Chamber: ["terp_torch", "purple_keycard"],
+  Cure_Vault: ["rare_seed_pack_bonus"],
+  Rosin_Reactor: ["rosin_railgun"],
+  Seed_Bank_Siege: ["gold_keycard", "trichome_reaper"],
+  Mold_Mothers_Den: ["episode_clear"]
+};
