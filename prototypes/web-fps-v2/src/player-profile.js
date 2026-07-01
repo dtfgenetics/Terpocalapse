caedcaabace9da1627796e7eef0929795882e60e
dtfgenetics <green801028@gmail.com>
2026-06-30 21:57:25 -0500

    Add V2 player profile

---
+++ prototypes/web-fps-v2/src/player-profile.js
@@ -0,0 +1,9 @@
+export const PLAYER_PROFILE = {
+  name: "Seed Runner",
+  role: "last grower inside the contaminated facility",
+  baseHealth: 100,
+  baseArmor: 0,
+  baseSpecial: 50,
+  movementStyle: "fast arcade strafing",
+  objective: "secure the seed vault"
+};
