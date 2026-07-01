80b7559a3a81532159954505b8d344bf8097e9ec
dtfgenetics <green801028@gmail.com>
2026-07-01 05:07:36 -0500

    Add spawn plan 06

---
+++ prototypes/web-fps-v2/src/spawn-plans/spawn-plan-06.js
@@ -0,0 +1,7 @@
+export const SPAWN_PLAN_06 = {
+  level: "Cure Vault",
+  threats: ["powdery_mildew_ghoul", "root_rot_crawler"],
+  pickups: ["rare_seed_pack", "cure_jar_health", "grow_light_overdrive"],
+  lore: ["note_cure_01"],
+  objective: "cure_vault_route"
+};
