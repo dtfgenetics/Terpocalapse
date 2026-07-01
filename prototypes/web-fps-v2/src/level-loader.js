d85521c77829906ce4692729e597792cbbd36091
dtfgenetics <green801028@gmail.com>
2026-07-01 05:10:53 -0500

    Normalize V2 level data in loader

--- prototypes/web-fps-v2/src/level-loader.js
+++ prototypes/web-fps-v2/src/level-loader.js
@@ -4,13 +4,39 @@ import { MISSION_BRIEFINGS } from "./story/mission-briefings.js";
 import { LEVEL_STORY_BEATS } from "./story/level-story-beats.js";
 
 export function loadLevelByIndex(index = 0) {
-  const map = MAP_REGISTRY[index] || MAP_REGISTRY[0];
-  const spawnPlan = SPAWN_REGISTRY.find((plan) => plan.level === map.title) || null;
-  const key = map.title.replaceAll(" ", "_").replaceAll("'", "");
+  const sourceMap = MAP_REGISTRY[index] || MAP_REGISTRY[0];
+  const level = normalizeMap(sourceMap);
+  const spawnPlan = SPAWN_REGISTRY.find((plan) => plan.level === level.name) || null;
+  const key = toStoryKey(level.name);
+
   return {
-    map,
+    level,
+    sourceMap,
     spawnPlan,
-    briefing: MISSION_BRIEFINGS[key] || map.notes || "Secure the route.",
+    briefing: MISSION_BRIEFINGS[key] || level.notes || "Secure the route.",
     story: LEVEL_STORY_BEATS[key] || null
   };
+}
+
+export function normalizeMap(sourceMap) {
+  const tileSize = sourceMap.tileSize || 64;
+  const start = sourceMap.start || { x: 1.5, y: 1.5, angle: 0 };
+
+  return {
+    id: sourceMap.id,
+    name: sourceMap.title,
+    goal: sourceMap.notes || "Reach the exit chamber.",
+    notes: sourceMap.notes || "",
+    tileSize,
+    map: sourceMap.layout || sourceMap.map || [],
+    playerStart: {
+      x: start.x * tileSize,
+      y: start.y * tileSize,
+      angle: start.angle || 0
+    }
+  };
+}
+
+export function toStoryKey(title) {
+  return title.replaceAll(" ", "_").replaceAll("'", "");
 }
