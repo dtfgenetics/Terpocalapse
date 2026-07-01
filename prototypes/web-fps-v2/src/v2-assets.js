94b83275fb8f246d9887c6afdad98ecde2b2c4a7
dtfgenetics <green801028@gmail.com>
2026-07-01 05:57:30 -0500

    Add V2 asset path registry

---
+++ prototypes/web-fps-v2/src/v2-assets.js
@@ -0,0 +1,22 @@
+export const V2_ASSETS = {
+  icons: {
+    health: "assets/icons/cure-jar-health.png",
+    armor: "assets/icons/kief-armor.png",
+    special: "assets/icons/grow-light-overdrive.png",
+    greenKey: "assets/icons/green-keycard.png"
+  },
+  tools: {
+    trim_shears: "assets/tools/trim-shears.png",
+    ph_blaster: "assets/tools/ph-blaster.png",
+    neem_cannon: "assets/tools/neem-cannon.png",
+    co2_burst_rifle: "assets/tools/co2-burst-rifle.png",
+    terp_torch: "assets/tools/terp-torch.png",
+    rosin_railgun: "assets/tools/rosin-railgun.png",
+    kief_grenades: "assets/tools/kief-grenades.png",
+    trichome_reaper: "assets/tools/trichome-reaper.png"
+  },
+  ui: {
+    titleLogo: "assets/ui/terpocalypse-logo.png",
+    panelFrame: "assets/ui/panel-frame.png"
+  }
+};
