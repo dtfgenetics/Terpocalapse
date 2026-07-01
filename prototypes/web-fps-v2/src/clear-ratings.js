8437c1b6c6d6eada160485496317acbbed0012f9
dtfgenetics <green801028@gmail.com>
2026-07-01 04:14:33 -0500

    Add V2 clear ratings

---
+++ prototypes/web-fps-v2/src/clear-ratings.js
@@ -0,0 +1,6 @@
+export const CLEAR_RATINGS = [
+  { grade: "S", label: "Master Grower", minScore: 9000 },
+  { grade: "A", label: "Clean Harvest", minScore: 7000 },
+  { grade: "B", label: "Stable Run", minScore: 5000 },
+  { grade: "C", label: "Barely Cured", minScore: 3000 }
+];
