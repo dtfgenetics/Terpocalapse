bb2d889524f8f240eec61e984052e74e887e150f
dtfgenetics <green801028@gmail.com>
2026-07-01 05:56:53 -0500

    Add V2 game event constants

---
+++ prototypes/web-fps-v2/src/game-events.js
@@ -0,0 +1,14 @@
+export const GAME_EVENTS = {
+  pickupCollected: "pickup_collected",
+  keycardCollected: "keycard_collected",
+  toolEquipped: "tool_equipped",
+  toolUsed: "tool_used",
+  threatCleared: "threat_cleared",
+  routeOpened: "route_opened",
+  loreFound: "lore_found",
+  healthLow: "health_low",
+  runFailed: "run_failed",
+  levelComplete: "level_complete",
+  storyPanelOpened: "story_panel_opened",
+  storyPanelClosed: "story_panel_closed"
+};
