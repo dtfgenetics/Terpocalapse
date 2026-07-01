2b4a8d84c48988c985505c1ff14ddd1a64225087
dtfgenetics <green801028@gmail.com>
2026-07-01 05:05:46 -0500

    Add V2 map 03

---
+++ prototypes/web-fps-v2/src/maps/map-03-nutrient-reservoir.js
@@ -0,0 +1,19 @@
+export const MAP_03_NUTRIENT_RESERVOIR = {
+  id: "map_03_nutrient_reservoir",
+  title: "Nutrient Reservoir",
+  tileSize: 64,
+  layout: [
+    "##############",
+    "#P...........#",
+    "#..####..##..#",
+    "#.....K......#",
+    "#..#.....#...#",
+    "#..#..S..#...#",
+    "#..####..#...#",
+    "#........D...#",
+    "#..........X.#",
+    "##############"
+  ],
+  start: { x: 1.5, y: 1.5, angle: 0 },
+  notes: "Reservoir route with switch room, feed tanks, and service lift exit."
+};
