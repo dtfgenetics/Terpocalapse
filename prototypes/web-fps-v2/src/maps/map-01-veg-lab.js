93c928ddbba529dc22f7f50c563b485eb9a281c4
dtfgenetics <green801028@gmail.com>
2026-07-01 04:16:31 -0500

    Add V2 map 01

---
+++ prototypes/web-fps-v2/src/maps/map-01-veg-lab.js
@@ -0,0 +1,19 @@
+export const MAP_01_VEG_LAB = {
+  id: "map_01_veg_lab",
+  title: "The Veg Lab",
+  tileSize: 64,
+  layout: [
+    "############",
+    "#..........#",
+    "#..P.......#",
+    "#..........#",
+    "#....K.....#",
+    "#..........#",
+    "#.....D....#",
+    "#..........#",
+    "#........X.#",
+    "############"
+  ],
+  start: { x: 2.5, y: 2.5, angle: 0 },
+  notes: "First route teaches pickup, green key, green door, and exit."
+};
