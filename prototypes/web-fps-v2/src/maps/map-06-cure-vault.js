d5ca7af733210605f5c31e5e083f6a072817907a
dtfgenetics <green801028@gmail.com>
2026-07-01 05:06:10 -0500

    Add V2 map 06

---
+++ prototypes/web-fps-v2/src/maps/map-06-cure-vault.js
@@ -0,0 +1,19 @@
+export const MAP_06_CURE_VAULT = {
+  id: "map_06_cure_vault",
+  title: "Cure Vault",
+  tileSize: 64,
+  layout: [
+    "##############",
+    "#P.....#.....#",
+    "#......#..S..#",
+    "#..##..#.....#",
+    "#..##........#",
+    "#......####..#",
+    "#..K.........#",
+    "#.......D....#",
+    "#..........X.#",
+    "##############"
+  ],
+  start: { x: 1.5, y: 1.5, angle: 0 },
+  notes: "Vault shelf route with humidity-control room and rare seed reward."
+};
