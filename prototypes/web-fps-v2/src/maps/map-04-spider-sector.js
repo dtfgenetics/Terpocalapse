2f7e226ae1e9293221b3dbf9b4dbe6cb97a0f05d
dtfgenetics <green801028@gmail.com>
2026-07-01 05:05:55 -0500

    Add V2 map 04

---
+++ prototypes/web-fps-v2/src/maps/map-04-spider-sector.js
@@ -0,0 +1,19 @@
+export const MAP_04_SPIDER_SECTOR = {
+  id: "map_04_spider_sector",
+  title: "Spider Mite Sector",
+  tileSize: 64,
+  layout: [
+    "##############",
+    "#P...........#",
+    "#..#######...#",
+    "#..#.....#...#",
+    "#..#..A..#...#",
+    "#..#.....#...#",
+    "#..###.###...#",
+    "#.....K..D...#",
+    "#..........X.#",
+    "##############"
+  ],
+  start: { x: 1.5, y: 1.5, angle: 0 },
+  notes: "Quarantine route with armor cache and sector-control exit."
+};
