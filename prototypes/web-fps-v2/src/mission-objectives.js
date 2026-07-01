909484af8cc16b92526275e268467481645d1227
dtfgenetics <green801028@gmail.com>
2026-06-30 21:59:14 -0500

    Add V2 mission objectives

---
+++ prototypes/web-fps-v2/src/mission-objectives.js
@@ -0,0 +1,23 @@
+export const MISSION_OBJECTIVES = {
+  The_Veg_Lab: [
+    "Pick up the pH Blaster",
+    "Find the Green Keycard",
+    "Open the green door",
+    "Reach the exit chamber"
+  ],
+  Clone_Room_Panic: [
+    "Move through the clone racks",
+    "Find the Neem Cannon",
+    "Reach the propagation exit"
+  ],
+  Nutrient_Reservoir: [
+    "Find the feed control switch",
+    "Cross the reservoir path",
+    "Reach the service lift"
+  ],
+  Seed_Bank_Siege: [
+    "Find the Gold Keycard",
+    "Reach the seed vault",
+    "Secure the genetics core"
+  ]
+};
