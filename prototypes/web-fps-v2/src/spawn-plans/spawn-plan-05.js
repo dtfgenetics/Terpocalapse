f7d8e1450f2eec0ed15d1ce87f0517e7c148da92
dtfgenetics <green801028@gmail.com>
2026-07-01 05:07:23 -0500

    Add spawn plan 05

---
+++ prototypes/web-fps-v2/src/spawn-plans/spawn-plan-05.js
@@ -0,0 +1,7 @@
+export const SPAWN_PLAN_05 = {
+  level: "Flower Chamber",
+  threats: ["bud_rot_brute", "powdery_mildew_ghoul"],
+  pickups: ["terp_torch", "purple_keycard"],
+  lore: ["note_flower_01"],
+  objective: "flower_route"
+};
