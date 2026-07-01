a7974c9f5b000b04f726fbdf0310e3c6d2c2a164
dtfgenetics <green801028@gmail.com>
2026-07-01 05:06:23 -0500

    Add V2 map 08

---
+++ prototypes/web-fps-v2/src/maps/map-08-seed-bank.js
@@ -0,0 +1,19 @@
+export const MAP_08_SEED_BANK = {
+  id: "map_08_seed_bank",
+  title: "Seed Bank Siege",
+  tileSize: 64,
+  layout: [
+    "################",
+    "#P.....#.......#",
+    "#......#..K....#",
+    "#..##..#.......#",
+    "#..##.....####.#",
+    "#......S.......#",
+    "#..######..##..#",
+    "#.........D....#",
+    "#............X.#",
+    "################"
+  ],
+  start: { x: 1.5, y: 1.5, angle: 0 },
+  notes: "Cold-storage route with gold access and genetics-core exit."
+};
