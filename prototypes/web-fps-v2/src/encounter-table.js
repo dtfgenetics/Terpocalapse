530257ff569e82083b3db739b7b00cb772525103
dtfgenetics <green801028@gmail.com>
2026-07-01 04:15:18 -0500

    Add V2 encounter table

---
+++ prototypes/web-fps-v2/src/encounter-table.js
@@ -0,0 +1,11 @@
+export const ENCOUNTER_TABLE = {
+  The_Veg_Lab: ["spider_mite_swarm", "powdery_mildew_ghoul", "nute_burn_sprayer"],
+  Clone_Room_Panic: ["spider_mite_swarm", "root_rot_crawler"],
+  Nutrient_Reservoir: ["nute_burn_sprayer", "powdery_mildew_ghoul"],
+  Spider_Mite_Sector: ["spider_mite_swarm", "root_rot_crawler", "bud_rot_brute"],
+  Flower_Chamber: ["bud_rot_brute", "powdery_mildew_ghoul", "compliance_drone"],
+  Cure_Vault: ["powdery_mildew_ghoul", "compliance_drone", "root_rot_crawler"],
+  Rosin_Reactor: ["nute_burn_sprayer", "compliance_drone"],
+  Seed_Bank_Siege: ["bud_rot_brute", "compliance_drone", "nute_burn_sprayer"],
+  Mold_Mothers_Den: ["mold_mother", "spider_mite_swarm", "nute_burn_sprayer"]
+};
