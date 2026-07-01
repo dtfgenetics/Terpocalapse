bbc586f43cda8f184a76c95e9a314f992edd623e
dtfgenetics <green801028@gmail.com>
2026-07-01 05:07:41 -0500

    Add spawn plan 07

---
+++ prototypes/web-fps-v2/src/spawn-plans/spawn-plan-07.js
@@ -0,0 +1,7 @@
+export const SPAWN_PLAN_07 = {
+  level: "Rosin Reactor",
+  threats: ["nute_burn_sprayer", "compliance_drone"],
+  pickups: ["rosin_railgun", "heavy_ammo_box"],
+  lore: ["note_reactor_01"],
+  objective: "reactor_route"
+};
