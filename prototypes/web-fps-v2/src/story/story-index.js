8bf2668b26591eece013c8d7f4aefa874f08553d
dtfgenetics <green801028@gmail.com>
2026-07-01 04:18:11 -0500

    Add story index

---
+++ prototypes/web-fps-v2/src/story/story-index.js
@@ -0,0 +1,4 @@
+export { INTRO_SEQUENCE } from "./intro-sequence.js";
+export { MISSION_BRIEFINGS } from "./mission-briefings.js";
+export { LEVEL_STORY_BEATS } from "./level-story-beats.js";
+export { LORE_NOTES } from "./lore-notes.js";
