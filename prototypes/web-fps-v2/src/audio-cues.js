4609a923b9373a0c0e941c1d7a2a1e987e782b09
dtfgenetics <green801028@gmail.com>
2026-07-01 05:08:42 -0500

    Add V2 audio cues

---
+++ prototypes/web-fps-v2/src/audio-cues.js
@@ -0,0 +1,7 @@
+export const AUDIO_CUES = {
+  menu: ["title_loop", "menu_select", "menu_confirm"],
+  player: ["pickup_item", "keycard_ready", "special_ready", "low_health"],
+  tools: ["trim_shears_use", "ph_blaster_use", "neem_cannon_use", "co2_burst_use", "terp_torch_use", "rosin_railgun_use"],
+  facility: ["door_open", "door_locked", "terminal_open", "mission_complete"],
+  ambient: ["grow_light_hum", "fan_loop", "reservoir_drip", "vault_hum", "reactor_pulse"]
+};
