0071beb93d7c18a65c72f9fe863c3a9b65da393f
dtfgenetics <green801028@gmail.com>
2026-06-30 21:57:39 -0500

    Add V2 level roadmap

---
+++ prototypes/web-fps-v2/src/level-roadmap.js
@@ -0,0 +1,11 @@
+export const LEVEL_ROADMAP = [
+  "The Veg Lab",
+  "Clone Room Panic",
+  "Nutrient Reservoir",
+  "Spider Mite Sector",
+  "Flower Chamber",
+  "Cure Vault",
+  "Rosin Reactor",
+  "Seed Bank Siege",
+  "Mold Mother's Den"
+];
