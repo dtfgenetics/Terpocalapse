e7335e8137b2a07fa9e952cde7430d467c53a2dd
dtfgenetics <green801028@gmail.com>
2026-07-01 04:12:08 -0500

    Add V2 level one plan

---
+++ prototypes/web-fps-v2/src/level-one-plan.js
@@ -0,0 +1,18 @@
+export const LEVEL_ONE_PLAN = {
+  title: "The Veg Lab",
+  mood: "safe start into contaminated grow lab pressure",
+  rooms: [
+    "Start Room",
+    "First Pest Room",
+    "Nutrient Storage",
+    "Green Keycard Room",
+    "Exit Chamber"
+  ],
+  goals: [
+    "Pick up the pH Blaster",
+    "Find the Green Keycard",
+    "Open the green door",
+    "Reach the exit chamber"
+  ],
+  rewards: ["Cure Jar Health", "Light Ammo Box", "Kief Armor"]
+};
