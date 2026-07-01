412bb066653c940286fc2215d6b83cedcdc28539
dtfgenetics <green801028@gmail.com>
2026-07-01 04:12:16 -0500

    Add V2 level two plan

---
+++ prototypes/web-fps-v2/src/level-two-plan.js
@@ -0,0 +1,17 @@
+export const LEVEL_TWO_PLAN = {
+  title: "Clone Room Panic",
+  mood: "tight humid propagation wing",
+  rooms: [
+    "Clone Rack Entry",
+    "Humidity Corridor",
+    "Tray Maze",
+    "Supply Shelf Secret",
+    "Propagation Exit"
+  ],
+  goals: [
+    "Move through the clone racks",
+    "Find the Neem Cannon",
+    "Open the propagation exit"
+  ],
+  rewards: ["Neem Cannon", "Heavy Ammo Box", "Rare Seed Pack"]
+};
