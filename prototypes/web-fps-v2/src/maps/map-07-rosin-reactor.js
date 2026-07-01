5ba631021fd45cc45a019c1836904eaacb3db691
dtfgenetics <green801028@gmail.com>
2026-07-01 05:06:16 -0500

    Add V2 map 07

---
+++ prototypes/web-fps-v2/src/maps/map-07-rosin-reactor.js
@@ -0,0 +1,19 @@
+export const MAP_07_ROSIN_REACTOR = {
+  id: "map_07_rosin_reactor",
+  title: "Rosin Reactor",
+  tileSize: 64,
+  layout: [
+    "################",
+    "#P.............#",
+    "#..####..####..#",
+    "#..#..S....#...#",
+    "#..#.......#...#",
+    "#.....K........#",
+    "#..####..####..#",
+    "#.........D....#",
+    "#............X.#",
+    "################"
+  ],
+  start: { x: 1.5, y: 1.5, angle: 0 },
+  notes: "Press-core route with timed switch planning and amber reactor exit."
+};
