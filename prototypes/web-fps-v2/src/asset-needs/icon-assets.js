964158b1f102ca9cf40f70019dec172798dfdc9a
dtfgenetics <green801028@gmail.com>
2026-07-01 05:08:24 -0500

    Add icon asset needs

---
+++ prototypes/web-fps-v2/src/asset-needs/icon-assets.js
@@ -0,0 +1,16 @@
+export const ICON_ASSETS = [
+  "icon_health_cure_jar",
+  "icon_armor_kief",
+  "icon_key_green",
+  "icon_key_purple",
+  "icon_key_gold",
+  "icon_special_grow_light",
+  "icon_tool_trim_shears",
+  "icon_tool_ph_blaster",
+  "icon_tool_neem_cannon",
+  "icon_tool_co2_burst",
+  "icon_tool_terp_torch",
+  "icon_tool_rosin_railgun",
+  "icon_tool_kief_grenade",
+  "icon_tool_trichome_reaper"
+];
