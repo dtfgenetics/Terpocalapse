d7bb7b8460ddd960b34e3e707dbd18576ec6b568
dtfgenetics <green801028@gmail.com>
2026-07-01 05:06:33 -0500

    Add V2 map 09

---
+++ prototypes/web-fps-v2/src/maps/map-09-mold-den.js
@@ -0,0 +1,19 @@
+export const MAP_09_MOLD_DEN = {
+  id: "map_09_mold_den",
+  title: "Mold Mother's Den",
+  tileSize: 64,
+  layout: [
+    "##################",
+    "#P...............#",
+    "#..######..####..#",
+    "#..#..........#..#",
+    "#..#....S.....#..#",
+    "#.......K........#",
+    "#..#..........#..#",
+    "#..######..####..#",
+    "#..........D...X.#",
+    "##################"
+  ],
+  start: { x: 1.5, y: 1.5, angle: 0 },
+  notes: "Final vault-core route with boss arena planning and episode clear gate."
+};
