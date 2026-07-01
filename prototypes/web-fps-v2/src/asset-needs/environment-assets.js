99d1e9c71e187cf01723426b568707d878a1d8ce
dtfgenetics <green801028@gmail.com>
2026-07-01 05:08:34 -0500

    Add environment asset needs

---
+++ prototypes/web-fps-v2/src/asset-needs/environment-assets.js
@@ -0,0 +1,11 @@
+export const ENVIRONMENT_ASSETS = {
+  shared: ["door_green", "door_purple", "door_gold", "exit_marker", "secret_marker"],
+  The_Veg_Lab: ["mylar_wall", "green_light", "wet_concrete", "clone_tray"],
+  Clone_Room_Panic: ["clone_dome", "mist_panel", "rack_light", "tray_shelf"],
+  Nutrient_Reservoir: ["feed_tank", "pipe_wall", "orange_warning_panel", "wet_floor"],
+  Flower_Chamber: ["canopy_wall", "purple_light", "cola_prop", "amber_trichome_panel"],
+  Cure_Vault: ["jar_shelf", "vault_wall", "humidity_panel", "gold_label_wall"],
+  Rosin_Reactor: ["press_plate", "amber_pipe", "steam_vent", "reactor_panel"],
+  Seed_Bank_Siege: ["seed_vault_wall", "cold_storage_door", "capsule_wall", "gold_door"],
+  Mold_Mothers_Den: ["mold_wall", "vault_core", "white_fog_panel", "cracked_lab_wall"]
+};
