6b6ee0bcb9b1caab15ca66d8de183d8c00242e0f
dtfgenetics <green801028@gmail.com>
2026-07-01 05:06:53 -0500

    Add spawn plan 02

---
+++ prototypes/web-fps-v2/src/spawn-plans/spawn-plan-02.js
@@ -0,0 +1,7 @@
+export const SPAWN_PLAN_02 = {
+  level: "Clone Room Panic",
+  threats: ["spider_mite_swarm", "root_rot_crawler"],
+  pickups: ["neem_cannon", "heavy_ammo_box", "rare_seed_pack"],
+  lore: ["note_clone_01"],
+  objective: "propagation_exit"
+};
