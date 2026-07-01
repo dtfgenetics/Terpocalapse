5da9698995441ca52b717e5fa3f52836f9465319
dtfgenetics <green801028@gmail.com>
2026-07-01 04:12:39 -0500

    Add V2 level four plan

---
+++ prototypes/web-fps-v2/src/level-four-plan.js
@@ -0,0 +1,17 @@
+export const LEVEL_FOUR_PLAN = {
+  title: "Spider Mite Sector",
+  mood: "quarantine wing with fast pest pressure",
+  rooms: [
+    "Quarantine Gate",
+    "Leaf Row Corridor",
+    "Vent Nest",
+    "Armor Cache",
+    "Sector Control"
+  ],
+  goals: [
+    "Enter the quarantine wing",
+    "Reach the armor cache",
+    "Open sector control"
+  ],
+  rewards: ["Kief Armor", "Light Ammo Box", "Golden Nug"]
+};
