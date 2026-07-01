6b588c7d1b74007c04a5b9fa90215ecc6ae6392c
dtfgenetics <green801028@gmail.com>
2026-07-01 05:07:10 -0500

    Add spawn plan 04

---
+++ prototypes/web-fps-v2/src/spawn-plans/spawn-plan-04.js
@@ -0,0 +1,7 @@
+export const SPAWN_PLAN_04 = {
+  level: "Spider Mite Sector",
+  threats: ["spider_mite_swarm", "root_rot_crawler", "bud_rot_brute"],
+  pickups: ["kief_armor", "light_ammo_box", "golden_nug"],
+  lore: ["note_pest_01"],
+  objective: "quarantine_route"
+};
