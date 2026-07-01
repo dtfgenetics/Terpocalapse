c9f7b23ea341cd8156e7a553f1df9f236f175d85
dtfgenetics <green801028@gmail.com>
2026-07-01 05:06:03 -0500

    Add V2 map 05

---
+++ prototypes/web-fps-v2/src/maps/map-05-flower-chamber.js
@@ -0,0 +1,19 @@
+export const MAP_05_FLOWER_CHAMBER = {
+  id: "map_05_flower_chamber",
+  title: "Flower Chamber",
+  tileSize: 64,
+  layout: [
+    "################",
+    "#P.............#",
+    "#..####..####..#",
+    "#..#.......#...#",
+    "#..#..K....#...#",
+    "#..#.......#...#",
+    "#..####..####..#",
+    "#.......S......#",
+    "#.........D..X.#",
+    "################"
+  ],
+  start: { x: 1.5, y: 1.5, angle: 0 },
+  notes: "Larger chamber route with purple access and canopy lift exit."
+};
