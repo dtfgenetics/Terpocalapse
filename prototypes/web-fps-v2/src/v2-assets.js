0b282e409b97b23b9dd7c439d95466d0b7b62ec3
dtfgenetics <green801028@gmail.com>
2026-09-09 05:54:47 -0500

    Align V2 asset paths with manifest

--- prototypes/web-fps-v2/src/v2-assets.js
+++ prototypes/web-fps-v2/src/v2-assets.js
@@ -1,19 +1,29 @@
 export const V2_ASSETS = {
   icons: {
-    health: "assets/icons/cure-jar-health.png",
-    armor: "assets/icons/kief-armor.png",
-    special: "assets/icons/grow-light-overdrive.png",
-    greenKey: "assets/icons/green-keycard.png"
+    health: "assets/pickups/cure-jar-health.png",
+    armor: "assets/pickups/kief-armor.png",
+    special: "assets/pickups/grow-light-overdrive.png",
+    greenKey: "assets/pickups/green-keycard.png"
   },
   tools: {
-    trim_shears: "assets/tools/trim-shears.png",
-    ph_blaster: "assets/tools/ph-blaster.png",
-    neem_cannon: "assets/tools/neem-cannon.png",
-    co2_burst_rifle: "assets/tools/co2-burst-rifle.png",
-    terp_torch: "assets/tools/terp-torch.png",
-    rosin_railgun: "assets/tools/rosin-railgun.png",
-    kief_grenades: "assets/tools/kief-grenades.png",
-    trichome_reaper: "assets/tools/trichome-reaper.png"
+    trim_shears: "assets/weapons/trim-shears.png",
+    ph_blaster: "assets/weapons/ph-blaster.png",
+    neem_cannon: "assets/weapons/neem-cannon.png",
+    co2_burst_rifle: "assets/weapons/co2-burst-rifle.png",
+    terp_torch: "assets/weapons/terp-torch.png",
+    rosin_railgun: "assets/weapons/rosin-railgun.png",
+    kief_grenades: "assets/weapons/kief-grenades.png",
+    trichome_reaper: "assets/weapons/trichome-reaper.png"
+  },
+  threats: {
+    spider_mite_swarm: "assets/enemies/spider-mite.png",
+    powdery_mildew_ghoul: "assets/enemies/mildew-ghoul.png",
+    nute_burn_sprayer: "assets/enemies/nute-burn-sprayer.png",
+    root_rot_crawler: "assets/enemies/root-rot-crawler.png",
+    bud_rot_brute: "assets/enemies/bud-rot-brute.png",
+    compliance_drone: "assets/enemies/compliance-drone.png",
+    aphid_queen: "assets/enemies/aphid-queen.png",
+    mold_mother: "assets/enemies/mold-mother.png"
   },
   ui: {
     titleLogo: "assets/ui/terpocalypse-logo.png",
