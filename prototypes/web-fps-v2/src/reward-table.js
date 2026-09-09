f7f0b472c1fc091d8c59fb0d1b7bdeb88e6460d7
dtfgenetics <green801028@gmail.com>
2026-09-09 11:50:56 -0500

    Add Kief Grenades to Cure Vault rewards

--- prototypes/web-fps-v2/src/reward-table.js
+++ prototypes/web-fps-v2/src/reward-table.js
@@ -4,7 +4,7 @@ export const REWARD_TABLE = {
   Nutrient_Reservoir: ["co2_burst_rifle"],
   Spider_Mite_Sector: ["kief_armor_bonus"],
   Flower_Chamber: ["terp_torch", "purple_keycard"],
-  Cure_Vault: ["rare_seed_pack_bonus"],
+  Cure_Vault: ["kief_grenades", "rare_seed_pack_bonus"],
   Rosin_Reactor: ["rosin_railgun"],
   Seed_Bank_Siege: ["gold_keycard", "trichome_reaper"],
   Mold_Mothers_Den: ["episode_clear"]
